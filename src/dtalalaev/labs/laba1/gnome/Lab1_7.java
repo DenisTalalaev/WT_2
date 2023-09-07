8c5ab73b4cd3413f2d17ff65ba54e263a5271234
talalaev <talalaev4@mail.ru>
2023-09-07 17:47:38 +0300

    lab 1 task 7 solved (Gnome sort)

---
+++ src/dtalalaev/labs/laba1/gnome/Lab1_7.java
@@ -0,0 +1,42 @@
+package dtalalaev.labs.laba1.gnome;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lab1_7 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Enter n - size of array");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("Enter elements of array");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        arr = gnomeSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int[] gnomeSort(int[] arr) {
+        int n = arr.length;
+        int index = 0;
+        while(index < n){
+            if(index == 0){
+                index++;
+            }
+            if(arr[index-1] <= arr[index]){
+                index++;
+            } else {
+                arr[index-1] = arr[index-1] ^ arr[index];
+                arr[index] = arr[index-1] ^ arr[index];
+                arr[index-1] = arr[index-1] ^ arr[index];
+                index--;
+            }
+        }
+        return arr;
+    }
+}
---
+++ src/dtalalaev/labs/laba1/gnome/Lab1_7Test.java
@@ -0,0 +1,12 @@
+package dtalalaev.labs.laba1.gnome;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Lab1_7Test {
+
+    @Test
+    void gnomeSort() {
+    }
+}
